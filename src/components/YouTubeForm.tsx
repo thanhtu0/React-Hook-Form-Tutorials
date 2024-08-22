531b020734e7b4994cf23fceedd73f2944001b4a
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-22 09:42:01 +0700

    feat: 8. Form Validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -23,28 +23,40 @@ export const YouTubeForm = () => {
 		<div>
 			<h1>YouTube Form {renderCount / 2}</h1>
 
-			<form onSubmit={handleSubmit(onSubmit)}>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				noValidate>
 				<label htmlFor='username'>Username</label>
 				<input
 					type='text'
 					id='username'
 					// name={name} ref={ref} onChange={onChange} onBlur={onBlur}
 					// được thay thế bằng cái ở dưới cho nó gọn gàng.
-					{...register('username')}
+					{...register('username', {
+						required: {
+							value: true,
+							message: 'Email is required',
+						},
+					})}
 				/>
 
 				<label htmlFor='email'>E-mail</label>
 				<input
 					type='email'
 					id='email'
-					{...register('email')}
+					{...register('email', {
+						pattern: {
+							value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+							message: 'Invalid email format',
+						},
+					})}
 				/>
 
 				<label htmlFor='channel'>Channel</label>
 				<input
 					type='text'
 					id='channel'
-					{...register('channel')}
+					{...register('channel', { required: 'Channel is required' })}
 				/>
 
 				<button>Submit</button>
