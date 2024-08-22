a51f96542599b2e5cc088a8e63a8801b949848d3
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-22 11:15:47 +0700

    feat: 15. Dynamic Fields

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
 let renderCount = 0;
@@ -12,6 +12,9 @@ type FormValues = {
 		facebook: string;
 	};
 	phoneNumbers: string[];
+	phNumbers: {
+		number: string;
+	}[];
 };
 
 export const YouTubeForm = () => {
@@ -25,6 +28,7 @@ export const YouTubeForm = () => {
 				facebook: '',
 			},
 			phoneNumbers: ['', ''],
+			phNumbers: [{ number: '' }],
 		},
 	});
 	// Hoặc
@@ -42,6 +46,11 @@ export const YouTubeForm = () => {
 	const { register, control, handleSubmit, formState } = form;
 	const { errors } = formState;
 	// const { name, ref, onChange, onBlur } = register('username');
+
+	const { fields, append, remove } = useFieldArray({
+		name: 'phNumbers',
+		control,
+	});
 
 	const onSubmit = (data: FormValues) => {
 		console.log('Form submitted', data);
@@ -146,12 +155,7 @@ export const YouTubeForm = () => {
 					<input
 						type='text'
 						id='primary-phone'
-						{...register('phoneNumbers.0', {
-							required: {
-								value: true,
-								message: 'Facebook is required',
-							},
-						})}
+						{...register('phoneNumbers.0')}
 					/>
 				</div>
 
@@ -160,13 +164,38 @@ export const YouTubeForm = () => {
 					<input
 						type='text'
 						id='secondary-phone'
-						{...register('phoneNumbers.1', {
-							required: {
-								value: true,
-								message: 'Facebook is required',
-							},
-						})}
+						{...register('phoneNumbers.1')}
 					/>
+				</div>
+
+				<div>
+					<label>List of phone numbers</label>
+					<div>
+						{fields.map((field, index) => {
+							return (
+								<div
+									className='form-control'
+									key={field.id}>
+									<input
+										type='text'
+										{...register(`phNumbers.${index}.number` as const)}
+									/>
+									{index > 0 && (
+										<button
+											type='button'
+											onClick={() => remove(index)}>
+											Remove
+										</button>
+									)}
+								</div>
+							);
+						})}
+						<button
+							type='button'
+							onClick={() => append({ number: '' })}>
+							Add phone number
+						</button>
+					</div>
 				</div>
 				<button>Submit</button>
 			</form>
