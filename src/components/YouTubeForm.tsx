de311f98c7cd82a4fc5bc72ddee11e2fa6afa984
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-22 15:47:10 +0700

    feat: 26. Async Validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -151,6 +151,13 @@ export const YouTubeForm = () => {
 								notBlackListed: (fieldValue) => {
 									return !fieldValue.endsWith('baddomain.com') || 'This domain is not supported';
 								},
+								emailAvailable: async (fieldValue) => {
+									const response = await fetch(
+										`https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+									);
+									const data = await response.json();
+									return data.length == 0 || 'Email already exists';
+								},
 							},
 						})}
 					/>
@@ -273,7 +280,7 @@ export const YouTubeForm = () => {
 					<p className='error'>{errors.dob?.message}</p>
 				</div>
 
-				<button disabled={!isDirty || !isValid || isSubmitting}>Submit</button>
+				<button disabled={!isDirty  || isSubmitting}>Submit</button>
 				<button
 					type='button'
 					onClick={() => reset()}>
